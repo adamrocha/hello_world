8fe34cb641ddcc3a612755b3f1fe97d2f34844fe
adamrocha <adamrocha13@gmail.com>
2018-10-02 19:59:27 -0700

    perms

--- ip_extract.sh
+++ ip_extract.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-# extract IP's from access logs
+#!/usr/bin/env bash
+# Extract IP's from access logs
 
 grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' /var/log/nginx/access.log |\
 sort |uniq -c |sort -rn
